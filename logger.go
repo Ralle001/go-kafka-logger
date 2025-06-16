cb5fc8508a26696c84fddf1a6f62714be102bf04
dtreszler <donattreszler@gistra.es>
2025-06-16 11:03:32 +0200

    implemented retry logic and error handling for mashal

--- logger.go
+++ logger.go
@@ -8,19 +8,31 @@ import (
 	"github.com/IBM/sarama"
 )
 
-func SendLogToKafka(producer sarama.SyncProducer, service, level, message string) {
+func SendLogToKafka(producer sarama.SyncProducer, service, level, message string) error {
 	logEntry := map[string]interface{}{
 		"level":     level,
 		"service":   service,
 		"message":   message,
 		"timestamp": time.Now().Format(time.RFC3339),
 	}
-	msgBytes, _ := json.Marshal(logEntry)
-	_, _, err := producer.SendMessage(&sarama.ProducerMessage{
-		Topic: "logs",
-		Value: sarama.ByteEncoder(msgBytes),
-	})
-	if err != nil {
-		log.Printf("Failed to send log message to Kafka: %v", err)
+	msgBytes, errorJson := json.Marshal(logEntry)
+	if errorJson != nil {
+		log.Printf("Failed to marshal log entry: %v", errorJson)
+		return errorJson
 	}
+
+	const maxRetries = 5
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		_, _, err = producer.SendMessage(&sarama.ProducerMessage{
+			Topic: "logs",
+			Value: sarama.ByteEncoder(msgBytes),
+		})
+		if err == nil {
+			return nil
+		}
+		log.Printf("Failed to send log message to Kafka: %v", err)
+		time.Sleep(time.Second * time.Duration(i)) // Exponential backoff
+	}
+	return err
 }
